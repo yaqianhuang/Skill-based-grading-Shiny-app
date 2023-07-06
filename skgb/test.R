2f07ee5c43643d4a8a284d8f1e926ecb1a03e113
yaqianhuang <39746750+yaqianhuang@users.noreply.github.com>
2023-07-06 04:12:51 -0700

    Delete skgb directory

--- skgb/test.R
+++
@@ -1,8 +0,0 @@
-plotstyle = list(
-  theme_light(base_size=32),
-  theme(panel.border = element_rect(color = 'black',fill=NA),
-        axis.title.y = element_blank())
-)
-ggplot(df, aes(y = skill,x=num)) +
-  geom_segment(aes(x=as.numeric(highlight),y=0,xend=as.numeric(highlight)+2,yend=0)) +
-  labs(x='# Times demonstrated')+plotstyle
